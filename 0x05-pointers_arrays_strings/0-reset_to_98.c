23658be8ecc9e8ce54e762f5e869fd36436f0f8d
engrismaildauda1 <ismail4dauda@gmail.com>
2023-02-27 07:51:36 -0800

    done

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value *n points to 98.
+ * @n: the number to modify.
+ *
+ * Return: success always
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
