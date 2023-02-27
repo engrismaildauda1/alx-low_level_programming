54a2960e82e119a2dde738978a89d8d3db5a3b28
engrismaildauda1 <ismail4dauda@gmail.com>
2023-02-27 08:00:32 -0800

    done

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,8 @@
-#ifndef main
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -13,5 +17,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-#define main
 #endif
