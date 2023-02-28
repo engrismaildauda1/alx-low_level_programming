4870d710a88696999185571d15a14be670d10e3b
engrismaildauda1 <ismail4dauda@gmail.com>
2023-02-27 22:38:33 -0800

    done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - main functions
+ *
+ * Return: always 0
+ */
+void print_alphabet(void);
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		_putchat(i);
+	_putchar('\n');
+}
